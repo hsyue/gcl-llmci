3216d6f4c7de8bade18633facc2cba9a59d7713d
huangshengyue <huangshengyue@126.com>
2025-06-12 02:13:48 +0800

    fix

--- plugin/plugin.go
+++ plugin/plugin.go
@@ -29,7 +29,7 @@ func (f *LLMCiPlugin) BuildAnalyzers() ([]*analysis.Analyzer, error) {
 	analyzer := &LLMCiAnalyzer{config: f.config}
 	return []*analysis.Analyzer{
 		{
-			Name: "-llmci",
+			Name: "llmci",
 			Doc:  "LLMCi Analyzer",
 			Run:  analyzer.run,
 		},
--- plugin/test_code.go
+++
@@ -1,8 +0,0 @@
-package plugin
-
-import "fmt"
-
-func main() {
-	var ptr *string
-	fmt.Println(*ptr) // 潜在的空指针解引用
-}
